a84f893486cfa4783005ef6cf16c1319504f9968
PaoloRollo <paolo.rollo1997@gmail.com>
2024-09-10 10:45:54 +0200

    feat: update with new page.

--- components/pages/landing-page-content.tsx
+++ components/pages/landing-page-content.tsx
@@ -2,7 +2,12 @@
 
 import { Button, Image, cn } from "@nextui-org/react";
 import { motion } from "framer-motion";
-import { GithubIcon, MapIcon, TwitterIcon } from "lucide-react";
+import {
+  GithubIcon,
+  MapIcon,
+  TwitterIcon,
+  FolderClockIcon,
+} from "lucide-react";
 import { Source_Code_Pro } from "next/font/google";
 import Link from "next/link";
 
@@ -60,8 +65,12 @@ export default function LandingPageContent() {
         whileInView={{ opacity: 1 }}
         viewport={{ once: true }}
         transition={{ duration: 1, delay: 2 }}
-        className="flex items-center space-x-4 mt-8"
+        className="flex items-center space-x-2 md:space-x-4 mt-8"
       >
+        <Button variant="faded" as={Link} href="/projects">
+          <FolderClockIcon />
+          <span className="hidden md:block">Projects</span>
+        </Button>
         <Button variant="faded" as={Link} href="/map">
           <MapIcon />
           <span className="hidden md:block">Map</span>
--- components/pages/projects-page-content.tsx
+++ components/pages/projects-page-content.tsx
@@ -21,20 +21,20 @@ interface Project {
 
 const projects: Project[] = [
   {
-    name: "Playerself Labs",
-    type: "startup",
-    description:
-      "Shaping the gaming industry through storytelling, technology and community.",
-    logo: "https://media.licdn.com/dms/image/D4D0BAQE5LCUBiZrYDg/company-logo_100_100/0/1689152492669/playerselfnft_logo?e=1710374400&v=beta&t=6bnzlg5xLoLEYD89nK5-138yh1Ry9UaLWW6Kf1WfNDw",
-    link: "https://playerself.com",
-  },
-  {
     name: "Brian",
     type: "startup",
     description:
       "Unleash the power of web3 Intents with AI. Swap, transfer, bridge tokens and perform any other on-chain operations by prompting.",
     logo: "https://www.brianknows.org/brian_logo.png",
     link: "https://brianknows.org",
+  },
+  {
+    name: "builders garden",
+    type: "startup",
+    description:
+      "A web3 MVP studio building and launching web3 products in a matter of weeks.",
+    logo: "/logos/builders-garden-logo.webp",
+    link: "https://builders.garden",
   },
   {
     name: "EthLogSpy",
@@ -45,11 +45,59 @@ const projects: Project[] = [
     link: "https://github.com/PaoloRollo/ethlogspy",
   },
   {
+    name: "Fluidpay",
+    type: "ETHGlobal London",
+    description:
+      "Fluidpay is a mobile-first p2p payments app running on Base and USDC-centric.",
+    logo: "/logos/fluidpay.svg",
+    link: "https://ethglobal.com/showcase/fluidpay-tpqvz",
+  },
+  {
+    name: "Papabase",
+    type: "ETHDenver 2024",
+    description:
+      "Making public goods donations accessible to everyone through crypto and fiat.",
+    logo: "/logos/papabase.svg",
+    link: "https://devfolio.co/projects/papabase-7d6b",
+  },
+  {
+    name: "Ghost",
+    type: "LFGHO Winner",
+    description:
+      "A mobile-native AA smart wallet designed for p2p payments, featuring automatic saving plans and easy-to-use investment features GHO-based.",
+    logo: "/logos/ghost.webp",
+    link: "https://ethglobal.com/showcase/ghost-mae3q",
+  },
+  {
     name: "Pulse",
-    type: "hackathon",
-    description: "",
-    logo: "",
-    link: "https://github.com/PaoloRollo/pulse",
+    type: "ETHGlobal Istanbul",
+    description:
+      "Pulse is a revolutionary mobile-first social client leveraging Lens and Farcaster frameworks.",
+    logo: "/logos/pulse.webp",
+    link: "https://pulse-indol.vercel.app/",
+  },
+  {
+    name: "threelingo",
+    type: "ETHLisbon",
+    description: "the free, fun and effective way to learn web3.",
+    logo: "/logos/threelingo.webp",
+    link: "https://threelingo.vercel.app/",
+  },
+  {
+    name: "urbe.eth",
+    type: "community",
+    description:
+      "Community for Web3 builders in rome: coworking, events, hackathons, and more.",
+    logo: "/logos/urbe-eth.webp",
+    link: "https://urbe.build",
+  },
+  {
+    name: "ETHRome",
+    type: "community",
+    description:
+      "The 1st web3 hackathon in Italy, on governance and privacy topics",
+    logo: "/logos/ethrome.webp",
+    link: "https://ethrome.org",
   },
 ];
 
@@ -65,7 +113,13 @@ export default function ProjectsPageContent() {
       >
         projects
       </motion.h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 mt-8 gap-4">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1, delay: 1 }}
+        className="grid grid-cols-1 md:grid-cols-3 mt-8 gap-4 mx-auto p-4"
+      >
         {projects.map((project) => (
           <Card className="max-w-[400px]" key={project.name}>
             <CardHeader className="flex justify-between">
@@ -78,25 +132,40 @@ export default function ProjectsPageContent() {
                   width={40}
                 />
                 <div className="flex flex-col">
-                  <p className="text-md">{project.name}</p>
+                  <p className="text-md font-bold">{project.name}</p>
                   <Link
                     href={project.link}
                     isExternal
-                    className="text-small text-default-500 truncate"
+                    className="text-xs text-default-500 truncate"
                   >
-                    {project.link}
+                    {project.link.length > 22
+                      ? `${project.link.slice(0, 22)}...`
+                      : project.link}
                   </Link>
                 </div>
               </div>
-              <Chip color="primary">{project.type}</Chip>
+              <Chip
+                color={
+                  project.type === "startup"
+                    ? "primary"
+                    : project.type === "community"
+                    ? "warning"
+                    : project.type === "open source"
+                    ? "success"
+                    : "danger"
+                }
+                size="sm"
+              >
+                {project.type}
+              </Chip>
             </CardHeader>
             <Divider />
             <CardBody>
-              <p>{project.description}</p>
+              <p className="text-sm">{project.description}</p>
             </CardBody>
           </Card>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 }
